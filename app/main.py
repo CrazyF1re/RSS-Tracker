61334d51764a928234f7286b3d4cbd5e809f78e7
Vladimir <vova.plisko@mail.ru>
2025-05-22 01:09:20 +0700

    setup main endpoint with template

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from typing import Union
-from fastapi.templating import Jinja2Templates
 from .routers import keywords, news, rss
 from dotenv import load_dotenv
-
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
@@ -13,13 +12,10 @@ load_dotenv()
 
 app.include_router(rss.router)
 app.include_router(keywords.router)
+app.include_router(news.router)
 
+templates = Jinja2Templates(directory="./app/templates")
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_root(request:Request):
+    return templates.TemplateResponse(request=request, name="main.html")
