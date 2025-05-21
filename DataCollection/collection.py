f81c9adb45de5cdf9d6aafed8e6a0a37b376b05b
Vladimir <vova.plisko@mail.ru>
2025-05-21 20:15:17 +0700

    add dotenv,  setup keywords table and function for delete/put data into table

--- DataCollection/collection.py
+++ DataCollection/collection.py
@@ -1,51 +1,102 @@
 #mysql
-from config import *
 import sqlite3
 import requests
 import os
+import json
 import xml.etree.ElementTree as ET
-import time
+from dotenv import load_dotenv
 
+# Load .env file
+load_dotenv()
+RSS = json.loads(os.environ['RSS'])
+KEYWORDS = json.loads(os.environ['KEYWORDS'])
+DB_NAME = os.environ["DB_NAME"]
+
+
+def singleton(class_):
+    instances = {}
+    def getinstance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+    return getinstance
+
+
+@singleton
 class Database:
     def __init__(self):
-        self.con = sqlite3.connect(DB_NAME)
-        self.cur = self.con.cursor()
-        self.cur.execute("""CREATE TABLE IF NOT EXISTS news (
+        self.__con = sqlite3.connect(DB_NAME)
+        self.__cur = self.__con.cursor()
+        self.__cur.execute("""CREATE TABLE IF NOT EXISTS news (
             title text PRIMARY KEY, 
             magazin text NOT NULL, 
             url text NOT NULL
         );""")
 
-        self.cur.execute("""CREATE TABLE IF NOT EXISTS rss(
+        self.__cur.execute("""CREATE TABLE IF NOT EXISTS rss(
             url text NOT NULL UNIQUE
                          )""")
-        tmp = [(i,) for i in RSS]
-        print(tmp)
-        self.con.executemany("""insert or ignore into rss(url) values(?)""", [(i,) for i in RSS])
-        self.con.commit()
-
-    def get_news(self):
-        lst = []
-        for blog in RSS:
-            print(blog)
-            resp = requests.get(blog)
-            time.sleep(1)
-            tree = ET.ElementTree(ET.fromstring(resp.text))
-            for i in tree.findall(".//channel/item"):
-                title = i.find("title").text
-                link = i.find("link").text
-                magazin = blog.split('.ru')[0]+".ru"
-                lst.append((title,magazin,link))
-        self.cur.executemany("""insert or ignore into news(title,magazin,url) values(?,?,?)""",lst)
-        self.con.commit()
-            
-            
         
+        self.__cur.execute("""CREATE TABLE IF NOT EXISTS keywords(
+            word text NOT NULL UNIQUE        
+        )""")
+        self.__con.commit()
 
-
-def main():
-    a = Database()
-    a.get_news()
+    def __get_news_rss(self,url):
+        print(url)
+        resp = requests.get(url)
+        resp.encoding = 'utf-8'
+        lst = []
+        tree = ET.ElementTree(ET.fromstring(resp.text))
+        for i in tree.findall(".//channel/item"):
+            title = i.find("title").text
+            link = i.find("link").text
+            magazin = url.split('.ru')[0]+".ru"
+            lst.append((title,magazin,link))
+        self.__cur.executemany("""insert or ignore into news(title,magazin,url) values(?,?,?)""",lst)
+        self.__con.commit()
     
-if __name__ == "__main__":
-    main()
+    def __del_news_rss(self,url):
+        magazin = url.split('.ru')[0]+".ru"
+        self.__cur.execute("""DELETE FROM news WHERE  magazin = ?""", (magazin,))
+        self.__con.commit()
+
+    def add_rss(self, rss:str):
+        self.__cur.execute("""INSERT OR IGNORE INTO rss values(?)""",(rss,))
+        self.__con.commit()
+        self.__get_news_rss(rss)
+
+    def del_rss(self, rss:list):
+        self.__cur.executemany("""DELETE FROM rss WHERE url = ?""", [(i,) for i in rss])
+        self.__con.commit()
+        for i in rss:
+            self.__del_news_rss(i)
+    
+    def setup_default_rss(self):
+        cur_rss  = [i[0] for i in self.get_rss_list()]
+        for i in RSS:
+            if i not in cur_rss:
+                self.__get_news_rss(i)
+        tmp = [(i,) for i in RSS]
+        self.__cur.executemany("""INSERT OR IGNORE INTO rss values(?)""",tmp)
+        self.__con.commit()
+
+    def get_rss_list(self):
+        return self.__cur.execute("""SELECT * from rss""").fetchall()
+    
+    def get_keywords_list(self):
+        return self.__cur.execute("""SELECT * from keywords""").fetchall()
+    
+    def setup_default_keywords(self):
+        tmp = [(i,) for i in KEYWORDS]
+        self.__cur.execute("""DELETE FROM keywords""")
+        self.__cur.executemany("""INSERT OR IGNORE INTO keywords values(?)""",tmp)
+        self.__con.commit()
+
+    def add_keywords(self, keywords:list):
+        self.__cur.executemany("""INSERT OR IGNORE into keywords values(?)""",[(i,) for i in keywords])
+        self.__con.commit()
+
+    def del_keywords(self, keywords:list):
+        self.__cur.executemany("""DELETE FROM keywords where word = ?""",[(i,) for i in keywords])
+        self.__con.commit()
