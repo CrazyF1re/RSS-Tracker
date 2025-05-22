bc17f4a481317d01bd0419c486cfa6c6ac9d2db0
Vladimir <vova.plisko@mail.ru>
2025-05-22 13:59:49 +0700

    add schedulable function to update database with news

--- app/main.py
+++ app/main.py
@@ -1,13 +1,48 @@
 from fastapi import FastAPI, Request
-from typing import Union
 from .routers import keywords, news, rss
 from dotenv import load_dotenv
 from fastapi.templating import Jinja2Templates
-
-app = FastAPI()
-
+from contextlib import asynccontextmanager
+from apscheduler.schedulers.background import BackgroundScheduler
+import sqlite3
+import os
+import requests
+import xml.etree.ElementTree as ET
 # Load .env file
 load_dotenv()
+
+def update_news():
+    con = sqlite3.connect(os.environ["DB_NAME"])
+    rss_list = con.cursor().execute("""SELECT * from rss""").fetchall()
+    rss_list = [i[0] for i in rss_list]
+    for url in rss_list:
+        resp = requests.get(url)
+        resp.encoding = 'utf-8'
+        lst = []
+        tree = ET.ElementTree(ET.fromstring(resp.text))
+        for i in tree.findall(".//channel/item"):
+            title = i.find("title").text
+            link = i.find("link").text
+            magazin = url.split('.ru')[0]+".ru"
+            lst.append((title,magazin,link))
+        con.cursor().executemany("""insert or ignore into news(title,magazin,url) values(?,?,?)""",lst)
+        con.commit()
+    con.close()
+    
+
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(update_news,"interval",minutes = 1)
+    scheduler.start()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+
+
+
 
 
 app.include_router(rss.router)
@@ -18,4 +53,4 @@ templates = Jinja2Templates(directory="./app/templates")
 
 @app.get("/")
 def read_root(request:Request):
-    return templates.TemplateResponse(request=request, name="main.html")
+    return templates.TemplateResponse(request=request, name="news.html")
